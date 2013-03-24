83873cde3a637bff042eb45eea7dfc1fb2717b56
Nick Rowe <nixterrimus@dcxn.com>
2013-03-23 23:57:36 -0700

    Clean state with working Ember

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '0af2d07b18f6ab4846bd23af9ad813a3985261df1e6e040085f45955dddef24ccc89026de5315525ec9c0597be89bb5460807e892bc0ada74780b59d766d02c9'
