49b32ee8df8c011f4d341b0b1c39ad142db24eec
Nick Rowe <nixterrimus@dcxn.com>
2013-03-27 19:27:12 -0700

    Add total computed property to day

--- app/assets/javascripts/models/day.js
+++ app/assets/javascripts/models/day.js
@@ -1,4 +1,9 @@
 App.Day = DS.Model.extend({
   entries:   DS.hasMany('App.Entry'),
-  goal:      DS.attr('number')
+  goal:      DS.attr('number'),
+
+  total: function() {
+    return 64;
+  }.property(),
+
 });
