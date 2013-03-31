c8d84661ce6eada060647e4da2e7d7da272bc8ec
Nick Rowe <nixterrimus@dcxn.com>
2013-03-30 17:13:58 -0700

    Add recorded at to entry

--- app/assets/javascripts/models/entry.js
+++ app/assets/javascripts/models/entry.js
@@ -1,3 +1,10 @@
 App.Entry = DS.Model.extend({
   day:    DS.belongsTo('App.Day'),
+  recordedAt:   DS.attr('date'),
+
+  daysAgo: function() {
+    now = new Date().getTime();
+    return Math.floor((now - this.get('recordedAt')) / 86400000);
+  }.property(),
+
 });
